cd99a265642ee7d44a391d6e14030c2a63b635f7
Roger Cordoba Garcia <rofacoga@gmail.com>
2015-11-13 14:47:40 -0500

    sistema de login

--- application/controllers/Principal.php
+++ application/controllers/Principal.php
@@ -3,8 +3,40 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Principal extends CI_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+ 
+		/*// Se carga el modelo de usuarios.
+		$this->load->model('users');
+ 
+		// Se carga el helper url y form.
+		$this->load->helper('url');
+		$this->load->helper('form');
+ 
+		// Se carga la libreria form_validation.
+		$this->load->library('form_validation');
+ 
+		// Se le asigna a la informacion a la variable $user.
+		$this->user = @$this->session->userdata('logged_user');
+		*/
+	}
+
 	public function index()
 	{
-		$this->load->view('principal_view.html');
+		if (isset($_POST['password'])) 
+		{
+
+			$this->load->model('principal_model');
+			$user=$_POST['username'];
+			$pass=$_POST['password'];
+			if( $this->principal_model->login($user,$pass) ){
+				redirect('welcome');
+			}else{
+				redirect('principal#Bad');
+			}
+		}
+		
+		$this->load->view('principal_view');
 	}
 }
--- application/models/principal_model.php
+++ application/models/principal_model.php
@@ -1,1 +1,28 @@
-<?php 
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+* 
+*/
+class principal_model extends CI_Model
+{
+    public function method_name()
+    {
+    	parent::__construct();
+    }
+
+    public function login( $username, $password)
+    {
+    	this->db->where('username', $username);
+    	this->db->where('password', $password);
+    	$q = this->db->get('usuario');
+
+    	if ($q->num_rows()>0) {
+    		return true;
+    	} else {
+    		return false;
+    	}
+    	
+    }
+
+}
