b245582aaaea152d8ecd981f372364e2eca2207e
Leydi <leydiviviana17@hotmail.com>
2015-11-13 16:46:56 -0500

    Pagina Sesion
    Se creo la página de inicio sesion a la aplicación web

---
+++ application/views/sesion_view.php
@@ -0,0 +1,73 @@
+<?php
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Pagina Principal</title>
+
+	<!-- Bootstrap -->
+    <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
+	<link href="<?php echo base_url(); ?>assets/css/estilos.css" rel="stylesheet"  type="text/css">
+
+</head>
+<body>
+	<br>
+
+	<div align="center">                         
+	    <a>
+	        <img src="assets/img/logo_uq.png"  class="img-responsive" >
+	    </a>
+    </div>
+
+		
+	<div class="fomu" align="center" >
+
+		
+		
+		
+		<section class="left">
+    		<form class="Sesion" action="" method="POST">
+							
+
+
+
+				<div class="form-group">
+					
+					<input class="btn btn-primary" type="submit" value="Mostrar Mapa"></input>
+				</div>
+
+				<div class="form-group" aling="center">
+					<input class="btn btn-primary" type="submit" value="Reservar Parqueadero"></input>
+				</div>
+
+				<div class="form-group" aling="center">
+					<input class="btn btn-primary" type="submit" value="Desocupar Parqueadero"></input>
+				</div>
+
+				<div class="form-group" aling="center">
+					<input class="btn btn-primary" type="submit" value="Parqueaderos Disponibles"></input>
+				</div>
+
+				<div class="" aling="center">
+					<input class="btn btn-primary" type="submit" value="Cerrar Sesion"></input>
+				</div>
+					
+
+			</form>
+		</section>
+
+		
+	</div>
+
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script> 
+</body>
+</html>
