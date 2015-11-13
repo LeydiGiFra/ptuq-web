7023cb2453fb234dccde6201c18c56e9f68d0d41
Roger Cordoba Garcia <rofacoga@gmail.com>
2015-11-13 16:57:31 -0500

    Eliminación de lineas de código sobrantes

--- application/controllers/Principal.php
+++ application/controllers/Principal.php
@@ -4,30 +4,40 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class principal extends CI_Controller {
 
-	public function __construct()
-	{
+	public function __construct() {
 		parent::__construct();
 	}
 
-	public function index()
-	{
-		if (isset($_POST['password'])) 
-		{
+	public function index() {
+
+		if( ! $this->session->userdata('username') ){
+			redirect('welcome');
+		}
+
+		if ( isset($_POST['password']) ) {
 
 			$this->load->model('principal_model');
+
+			$user=$_POST['username'];
+			$pass=$_POST['password'];
 			
-			if($this->principal_model->login($_POST['username'], $_POST['password']))
-			{
+			if( $this->principal_model->login($user, $pass) ) {
+				
+				$this->session->set_userdata('username', $user);
 				redirect('welcome');
 			}
-			else
-			{
-				redirect('principal_view#bad-password');
+			else {
+				redirect('principal#bad-password');
 			}
 			
 
 		}
 		
 		$this->load->view('principal_view');
+	}
+
+	public function logout(){
+		$this->session->sess_destroy();
+		redirect('principal');
 	}
 }
--- application/models/principal_model.php
+++ application/models/principal_model.php
@@ -6,8 +6,6 @@ if( ! defined('BASEPATH')) exit('No direct script access allowed');
 */
 class Principal_model extends CI_Model
 {
-    public $variable; 
-
     public function __construct()
     {
     	parent::__construct();
