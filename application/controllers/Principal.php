ae3c068da386051778ed2385aa30c1d4a5ce308d
Roger Cordoba Garcia <rofacoga@gmail.com>
2015-11-13 16:16:52 -0500

    eliminar lineas de código sobrantes

--- application/controllers/Principal.php
+++ application/controllers/Principal.php
@@ -7,20 +7,6 @@ class principal extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
- 
-		/*// Se carga el modelo de usuarios.
-		$this->load->model('users');
- 
-		// Se carga el helper url y form.
-		$this->load->helper('url');
-		$this->load->helper('form');
- 
-		// Se carga la libreria form_validation.
-		$this->load->library('form_validation');
- 
-		// Se le asigna a la informacion a la variable $user.
-		$this->user = @$this->session->userdata('logged_user');
-		*/
 	}
 
 	public function index()
